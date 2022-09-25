7732ab40dff73f7bdd1a5eef527f040dd5397264
ibragim <ibragim@MacBook-Air-ibragim.local>
2022-09-25 21:15:13 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:taxi_service_imitation/cubit/driver_selection/driver_selection_cubit.dart';
 import 'package:taxi_service_imitation/cubit/taxi_app_cubit.dart';
-
 import 'package:taxi_service_imitation/screens/home_page.dart';
-import 'package:taxi_service_imitation/screens/order_fulfillment_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,30 +16,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // ignore: deprecated_member_use
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ));
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.amber,
-          primaryIconTheme: const IconThemeData(color: Colors.red),
-          iconTheme: const IconThemeData(color: Colors.grey),
-          floatingActionButtonTheme: const FloatingActionButtonThemeData(
-              elevation: 8, backgroundColor: Colors.amberAccent),
-          primaryTextTheme: const TextTheme(
-            bodyText2: TextStyle(fontSize: 38, fontFamily: "Handwritten"),
-            bodyText1: TextStyle(
-              fontSize: 24,
-              color: Colors.black,
-            ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        primaryIconTheme: const IconThemeData(color: Colors.red),
+        iconTheme: const IconThemeData(color: Colors.grey),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            elevation: 8, backgroundColor: Colors.amberAccent),
+        primaryTextTheme: const TextTheme(
+          bodyText2: TextStyle(fontSize: 38, fontFamily: "Handwritten"),
+          bodyText1: TextStyle(
+            fontSize: 24,
+            color: Colors.black,
           ),
         ),
-        home: HomePage(
-          driverSelectionCubit: driverSelectionCubit,
-          taxiAppCubit: taxiAppCubit,
-        ));
+      ),
+      home: HomePage(
+        driverSelectionCubit: driverSelectionCubit,
+        taxiAppCubit: taxiAppCubit,
+      ),
+    );
   }
 }
